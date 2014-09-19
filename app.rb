8099294b82d97b5caf1cb3b03c3cc24a267334b9
Jakub Witkiewicz <krow10@gmail.com>
2014-09-19 04:07:44 +0100

    first

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require "sinatra/reloader"
+require 'mongo'
+
+
+def mongo 
+	begin
+		uri  = URI.parse(ENV['MONGOLAB_URI'])
+		conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+	    conn.db(uri.path.gsub(/^\//, ''))	
+	rescue
+		Mongo::Connection.new.db('doomp')
+	end
+end
+
+def store
+	mongo.collection("store")	
+end	
+
+
+get '/' do
+   redirect "/#{('a'..'z').to_a.shuffle.first(6).join}"
+end
+
+get '/:name' do
+   @doomp = store.find_one( name: params[:name] ) || {'content' => ''}
+   erb :default
+end	
+
+get "/:name/raw" do
+   doomp = store.find_one( name: params[:name] ) || {'content' => ''}
+   doomp['content']   
+end	
+
+
+
+post '/:name' do
+	store.update( 
+		{ name:  params[:name] },  
+		{ '$set' => { name: params[:name], content: params[:content] }}, 
+		{ upsert: true} 
+	)
+	"OK"
+end
+
+
+
+
+
+
