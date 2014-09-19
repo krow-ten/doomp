f893e692b29f63c2781eff7a0313c201dd080aa2
Jakub Witkiewicz <krow10@gmail.com>
2014-09-19 04:14:33 +0100

    fixing indentTION

--- app.rb
+++ app.rb
@@ -4,43 +4,42 @@ require 'mongo'
 
 
 def mongo 
-	begin
-		uri  = URI.parse(ENV['MONGOLAB_URI'])
-		conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-	    conn.db(uri.path.gsub(/^\//, ''))	
-	rescue
-		Mongo::Connection.new.db('doomp')
-	end
+  begin
+    uri  = URI.parse(ENV['MONGOLAB_URI'])
+    conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    conn.db(uri.path.gsub(/^\//, ''))   
+  rescue
+    Mongo::Connection.new.db('doomp')
+  end
 end
 
 def store
-	mongo.collection("store")	
-end	
+  mongo.collection("store")   
+end 
 
 
 get '/' do
-   redirect "/#{('a'..'z').to_a.shuffle.first(6).join}"
+  redirect "/#{('a'..'z').to_a.shuffle.first(6).join}"
 end
 
 get '/:name' do
-   @doomp = store.find_one( name: params[:name] ) || {'content' => ''}
-   erb :default
-end	
+  @doomp = store.find_one( name: params[:name] ) || {'content' => ''}
+  erb :default
+end 
 
 get "/:name/raw" do
-   doomp = store.find_one( name: params[:name] ) || {'content' => ''}
-   doomp['content']   
-end	
-
+  doomp = store.find_one( name: params[:name] ) || {'content' => ''}
+  doomp['content']   
+end 
 
 
 post '/:name' do
-	store.update( 
-		{ name:  params[:name] },  
-		{ '$set' => { name: params[:name], content: params[:content] }}, 
-		{ upsert: true} 
-	)
-	"OK"
+  store.update( 
+    { name:  params[:name] },  
+    { '$set' => { name: params[:name], content: params[:content] }}, 
+    { upsert: true} 
+  )
+  "OK"
 end
 
 
