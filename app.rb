44b6fec330f4dba03f3f6486ef2bb506e4f5146f
Jakub Witkiewicz <krow10@gmail.com>
2014-09-20 14:16:41 +0100

    security

--- app.rb
+++ app.rb
@@ -24,20 +24,47 @@ end
 
 get '/:name' do
   @doomp = store.find_one( name: params[:name] ) || {'content' => ''}
+  protected! @doomp
   erb :default
 end
 
 get "/:name/raw" do
   doomp = store.find_one( name: params[:name] ) || {'content' => ''}
+  protected! doomp
   doomp['content']
 end
 
 
 post '/:name' do
+  doomp = store.find_one( name: params[:name] )
+  protected! doomp
   store.update(
     { name:  params[:name] },
     { '$set' => { name: params[:name], content: CGI.escapeHTML(params[:content]) }},
     { upsert: true}
   )
   "OK"
+end
+
+get "/:name/protect" do
+  store.update(
+    { name:  params[:name] },
+    { '$set' => { protected: true }}
+  )
+  redirect "/#{params[:name]}"
+end
+
+
+
+helpers do
+  def protected! doomp
+    return if (doomp && !doomp['protected']) || authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['', ENV['PASSWORD']]
+  end
 end
